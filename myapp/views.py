2ca9c802a68e665fd51b8bd97439102910a6a5ba
Sasmita <sasmitab88@gmail.com>
2024-06-07 21:44:33 -0400

    Lab 5- Part 1
    Added views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Publisher
 
-# Create your views here.
+
+def index(request):
+    response = HttpResponse()
+
+    # Get the list of books ordered by primary key
+    booklist = Book.objects.all().order_by('id')
+
+    # Get the list of publishers ordered by city name in descending order
+    publisherlist = Publisher.objects.all().order_by('-city')
+
+    # Add books to response
+    response.write('<h1>List of Books</h1>')
+    for book in booklist:
+        response.write(f'<p>{book.id}. {book.title}</p>')
+
+    # Add publishers to response
+    response.write('<h1>List of Publishers</h1>')
+    for publisher in publisherlist:
+        response.write(f'<p>{publisher.name} located in {publisher.city}</p>')
+
+    return response
+
+

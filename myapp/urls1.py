60515eaa36bac90a1443e7d621878d4f5a6775ae
Sasmita <sasmitab88@gmail.com>
2024-06-23 21:47:05 -0400

    Checkin

--- myapp/urls1.py
+++ myapp/urls1.py
@@ -6,4 +6,5 @@ app_name = 'myapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('<int:book_id>/', views.detail, name='detail'),  # URL pattern for book details
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from .models import Book, Publisher
-
+from .models import Book, Publisher, Member
+from django.shortcuts import get_object_or_404
 
 def index(request):
     response = HttpResponse()
@@ -28,4 +28,15 @@ def about(request):
     heading1 = '<p>' + 'This is an eBook Website ' + '</p>'
     response.write(heading1)
     return response
+
+def detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id) #object for getting the details or not found
+    details_response = HttpResponse()
+    details_response.write('<h1>Details of the book with id #' + str(book_id) + '</h1>') #printing the bookid
+    book_name = book.title.upper() #book name in upper case
+    book_price = book.price #book price
+    book_publisher = book.publisher.name #publisher name
+    details_response.write('<p> The ' + book_name + ' costs $' + str(book_price) + ' and, published by '
+                           + book_publisher + '</p>')
+    return details_response
 

177251f127cf98cab68b8f6bc0dd838fd6d318d5
Singhmeghna <48406875+Singhmeghna@users.noreply.github.com>
2024-07-06 22:45:21 -0400

    Part 2 - Add review functionality and validation

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Publisher, Book, Member, Order
+from .models import Publisher, Book, Member, Order, Review
 
 # Register your models here.
 admin.site.register(Publisher)
 admin.site.register(Book)
 admin.site.register(Member)
 admin.site.register(Order)
+admin.site.register(Review)
 
